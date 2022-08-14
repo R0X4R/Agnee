66d4a23edc601c69fd3491c8c82de96c103a8bcf
Eshan Singh <32596297+R0X4R@users.noreply.github.com>
2022-08-14 19:00:20 +0530

    Agnee
    Find sensitive information using Dorks

--- README.md
+++ README.md
@@ -1,2 +1,96 @@
-# Agnee
- Find sensitive information using Dorks
+<h1 align="center">
+  <br>
+  <a href="https://github.com/R0X4R/Agnee"><img src=".github/static/logo.png" width="60%" alt="Agnee logo"></a>
+</h1>
+
+<h4 align="center"><b>Find sensitive information using dorks from different search-engines.</b></h4><br>
+
+<p align="center">
+  <a href="https://github.com/R0X4R/Agnee/releases">
+    <img src="https://img.shields.io/github/release/R0X4R/Agnee.svg?label=version">
+  </a>
+  <a href="#"><img src="https://madewithlove.org.in/badge.svg"></a>
+<a href="https://twitter.com/R0X4R/"><img src="https://img.shields.io/badge/twitter-%40R0X4R-blue.svg"></a>
+<a href="https://github.com/R0X4R/Agnee/issues"><img src="https://img.shields.io/badge/contributions-welcome-brightgreen.svg?style=flat"></a>
+<a href="https://github.com/R0X4R/Agnee/blob/main/LICENSE"><img src="https://img.shields.io/badge/License-MIT-yellow.svg"></a>
+<a href="https://github.com/R0X4R?tab=followers"><img src="https://img.shields.io/badge/github-%40R0X4R-orange"></a>
+  <a href="https://github.com/R0X4R/Agnee/issues?q=is%3Aissue+is%3Aclosed">
+      <img src="https://img.shields.io/github/issues-closed-raw/R0X4R/Agnee?color=dark-green&label=issues%20fixed">
+  </a>
+  <a href="https://travis-ci.com/R0X4R/Agnee">
+      <img src="https://img.shields.io/travis/com/R0X4R/Agnee.svg?color=dark-green&label=tests">
+  </a>
+</p>
+
+```console
+$ agnee -h
+
+Find sensitive information using Dorks.
+
+USAGE:
+    agnee -d domain.tld
+
+OPTIONS:
+    -d DOMAIN, --domain DOMAIN  Define the domain to scan
+    -a, --all                   Use all search-engines (default "Bing")
+    -e ENGINE, --engine ENGINE  List of search-engines to use [-e "bing, google"] (comma separated)
+    -h, --help                  show this help message and exit
+    -o OUTPUT, --output OUTPUT  Save the results in the output file
+    -p PAGE, --page PAGE        Number of pages to scrape
+    -q, --quite                 Suppress all the output
+```
+
+---
+
+Agnee uses [`search_engines`](https://github.com/tasos-py/Search-Engines-Scraper) to find sensitive information about given domain using multiple dorks through mutltiple search-engines. I have modified some code of `search_engines` and used it in this script to get the custom results and currently it only find results from bing, google and yahoo (will implement more in future for sure).
+
+### Installation
+
+<br>
+
+```console
+sudo pip3 install git+https://github.com/R0X4R/Search-Engines-Scraper.git && pip3 install agnee
+```
+
+> **Note**: Before installing agnee you must need git and python3 installed on your system.
+
+### Usage
+
++ **Simple usage arguments**
+    
+    ```console
+    agnee -d testphp.vulnweb.com
+
+    https://www.exploit-db.com/ghdb/6630
+    https://www.exploit-db.com/ghdb/5665
+    http://testphp.vulnweb.com/secured/phpinfo.php
+    ```
+
++ **To use a specific engine**
+
+    ```console
+    agnee -d testphp.vulnweb.com -e yahoo
+
+    Yahoo Dork: inurl:"/.git" testphp.vulnweb.com -github
+
+    https://www.exploit-db.com/ghdb/6630
+    https://www.exploit-db.com/ghdb/5665
+    http://testphp.vulnweb.com/secured/phpinfo.php
+    ```
+
++ **To search specific number of pages**
+    ```console
+    agnee -d testphp.vulnweb.com -p 1
+
++ **Use all the search-engines**
+
+    ```console
+    agnee -d testphp.vulnweb.com -all
+    ```
+    > **Note**: Sometimes google may block your requests so you need to wait for few hours.
+<br>
+
+### Donate
+If this tool helped you or you like my work
+
+</br><a href="https://www.buymeacoffee.com/R0X4R"><img src="https://img.buymeacoffee.com/button-api/?text=Help me to buy oscp&emoji=😇&slug=R0X4R&button_colour=5F7FFF&font_colour=ffffff&font_family=Cookie&outline_colour=000000&coffee_colour=FFDD00"/></a> <a style=" width: 135px; background-color: #1065b7; text-align: center; font-weight: 800; padding: 11px 0px; color: white; font-size: 12px; display: inline-block; text-decoration: none; " href='https://pmny.in/bIKNZngt4ys1'> Donate Now </a>   <a href="https://ko-fi.com/i/IK3K34SJSA"><img src="https://ko-fi.com/img/githubbutton_sm.svg"></a><br/><br/>
---
+++ agnee.py
@@ -0,0 +1,207 @@
+#!/usr/bin/env python3
+"""
+AGNEE v1.0
+AUTHOR: ESHAN SINGH (R0X4R)
+
+CREDITS https://github.com/tasos-py FOR THE SEARCH_ENGINES LIBRARY
+"""
+
+#@> IMPORTING ALL DEPENDENCIES
+from search_engines import Google, Bing, Yahoo, config
+from tldextract import extract
+from sys import stdout, exit
+from random import choice
+from time import sleep
+import argparse
+
+def parse_args():
+    """
+    Parse command line arguments
+    """
+    parser = argparse.ArgumentParser(description="Find sensitive information using Dorks.", usage="agnee -d domain.tld")
+    parser._optionals.title = "OPTIONS"
+
+    parser.add_argument("-d", "--domain", dest="domain", help="Define the domain to scan", required=True)
+    parser.add_argument("-a", "--all", dest="all", help="Use all search-engines (default \"Bing\")", action='store_true')
+    parser.add_argument("-e", "--engine", dest="engine", help="List of search-engines to use [-e \"bing, google\"] (comma separated)", default="bing")
+    parser.add_argument("-o", "--output", dest="output", help="Save the results in the output file")
+    parser.add_argument("-p", "--page", dest="page", help="Number of pages to scrape", default=config.SEARCH_ENGINE_RESULTS_PAGES, type=int)
+    parser.add_argument("-q", "--quite", dest="quite", help="Suppress all the output", action='store_true')   
+    
+    return parser.parse_args()
+
+cmd = parse_args() 
+
+if not cmd.engine is None or not cmd.all:
+    engines = [
+            e.strip() for e in cmd.engine.lower().split(',')
+        ]
+else:
+    print("Please choose an engine or use --all (agnee -h)")
+    exit(127)
+
+if cmd.quite:
+    if cmd.output is None:
+        print("Please specify the output file using -o/--output")
+        exit(127)
+
+"""Declaring variables"""
+odomain = extract(str(cmd.domain)).domain
+bsearch = Bing()
+# dsearch = Duckduckgo()
+gsearch = Google()
+ysearch = Yahoo()
+
+"""User-agent list (random library will choose it randomly)"""
+agntList = [
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36",
+    "Mozilla/5.0 (X11; Ubuntu; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2919.83 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2866.71 Safari/537.36",
+    "Mozilla/5.0 (X11; Ubuntu; Linux i686 on x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2820.59 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2762.73 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:77.0) Gecko/20190101 Firefox/77.0",
+    "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:77.0) Gecko/20100101 Firefox/77.0",
+    "Mozilla/5.0 (X11; Linux ppc64le; rv:75.0) Gecko/20100101 Firefox/75.0",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:39.0) Gecko/20100101 Firefox/75.0",
+    "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.10; rv:75.0) Gecko/20100101 Firefox/75.0",
+    "Opera/9.80 (X11; Linux i686; Ubuntu/14.10) Presto/2.12.388 Version/12.16.2",
+    "Opera/9.80 (X11; Linux i686; Ubuntu/14.10) Presto/2.12.388 Version/12.16",
+    "Opera/9.80 (Macintosh; Intel Mac OS X 10.14.1) Presto/2.12.388 Version/12.16",
+    "Opera/9.80 (Windows NT 6.0) Presto/2.12.388 Version/12.14",
+    "Chrome (AppleWebKit/537.1; Chrome50.0; Windows NT 6.3) AppleWebKit/537.36 (KHTML like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393",
+]
+uagents = choice(agntList)
+
+"""List of all dorks"""
+dorks = [
+    "inurl:\"/.git\" "+cmd.domain+" -github",
+    "site:"+cmd.domain+" ext:bkf | ext:bkp | ext:bak | ext:old | ext:backup",
+    "site:"+cmd.domain+" ext:sql | ext:dbf | ext:mdb",
+    "site:"+cmd.domain+" intitle:index.of | ext:log | ext:php intitle:phpinfo \"published by the PHP Group\" | inurl:shell | inurl:backdoor | inurl:wso | inurl:cmd | shadow | passwd | boot.ini | inurl:backdoor | inurl:readme | inurl:license | inurl:install | inurl:setup | inurl:config | inurl:\"/phpinfo.php\" | inurl:\".htaccess\" | ext:swf",
+    "site:"+cmd.domain+" intext:\"sql syntax near\" | intext:\"syntax error has occurred\" | intext:\"incorrect syntax near\" | intext:\"unexpected end of SQL command\" | intext:\"Warning: mysql_connect()\" | intext:\"Warning: mysql_query()\" | intext:\"Warning: pg_connect()\"",
+    "site:"+cmd.domain+" ext:doc | ext:docx | ext:odt | ext:pdf | ext:rtf | ext:sxw | ext:psw | ext:ppt | ext:pptx | ext:pps | ext:csv",
+    "site:"+cmd.domain+" ext:xml | ext:conf | ext:cnf | ext:reg | ext:inf | ext:rdp | ext:cfg | ext:txt | ext:ora | ext:env | ext:ini",
+    "site:"+cmd.domain+" \"PHP Parse error\" | \"PHP Warning\" | \"PHP Error\"",
+    "site:"+cmd.domain+" inurl:wp-content | inurl:wp-includes",
+    "inurl:\"/sym404/root/\" \""+cmd.domain+"\"",
+    "site:"+cmd.domain+" not for distribution | confidential | “employee only” | proprietary | top secret | classified | trade secret | internal | private | WS_FTP | ws_ftp | log | LOG",
+    "site:.s3.amazonaws.com | site:storage.googleapis.com | site:amazonaws.com \""+odomain+"\"",
+    "intitle:traefik inurl:8080/dashboard \""+odomain+"\"",
+    "intitle:\"Dashboard [Jenkins]\" \""+odomain+"\"",
+    "site:sharecode.io | site:controlc.com | site:codepad.co |site:ideone.com | site:codebeautify.org | site:jsdelivr.com | site:codeshare.io | site:codepen.io | site:repl.it | site:jsfiddle.net \""+odomain+"\"",
+    "site:gitter.im | site:papaly.com | site:productforums.google.com | site:coggle.it | site:replt.it | site:ycombinator.com | site:libraries.io | site:npm.runkit.com | site:npmjs.com | site:scribd.com \""+odomain+"\"",
+    "site:stackoverflow.com \""+cmd.domain+"\"",
+    "site:justpaste.it | site:heypasteit.com | site:pastebin.com \""+odomain+"\"",
+    "site:"+cmd.domain+" ext:action | ext:struts | ext:do",
+]
+
+def saveFile(file, result):
+    """
+    Save the output in a file
+    """
+    file = open((file), "a")
+    file.write(str(result))
+    file.write('\n')
+    file.close()
+
+def googSearch():
+    """
+    Google search function
+    """
+    for dork in dorks:
+        gdork = '\n' + "Google Dork: %s" % (str(dork)) + '\n'
+        if cmd.output is not None:
+            saveFile(cmd.output, gdork)
+        if not cmd.quite:
+            stdout.write(str(gdork) + '\n')
+        gsearch.set_headers({'User-Agent': uagents})
+        gout = gsearch.search(str(dork), cmd.page)
+        for links in gout.links():
+            if cmd.output is not None:
+                stdout.write(str(links) + '\n')
+                saveFile(cmd.output, links)
+            elif cmd.quite:
+                saveFile(cmd.output, links)
+            else:
+                stdout.write(str(links) + '\n')
+        sleep(2)
+
+def bingSearch():
+    """
+    Bing search function
+    """
+    for dork in dorks:
+        bdork = '\n' + "Bing Dork: %s" % (str(dork)) + '\n'
+        if cmd.output is not None:
+            saveFile(cmd.output, bdork)
+        if not cmd.quite:
+            stdout.write(str(bdork) + '\n')
+        bsearch.set_headers({'User-Agent': uagents})
+        bout = bsearch.search(str(dork), cmd.page)
+        for links in bout.links():
+            if cmd.output is not None:
+                stdout.write(str(links) + '\n')
+                saveFile(cmd.output, links)
+            elif cmd.quite:
+                saveFile(cmd.output, links)
+            else:
+                stdout.write(str(links) + '\n')
+        sleep(2)
+
+# def duckSearch():
+#     for dork in dorks:
+#         dsearch.set_headers({'User-Agent': uagents})
+#         dout = dsearch.search(str(dork), cmd.page)
+#         for links in dout.links():
+#             stdout.write(str(links) + '\n')
+#         sleep(2)
+
+def yahoSearch():
+    """
+    Yahoo search function
+    """
+    for dork in dorks:
+        ydork = '\n' + "Yahoo Dork: %s" % (str(dork)) + '\n'
+        if cmd.output is not None:
+            saveFile(cmd.output, ydork)
+        if not cmd.quite:
+            stdout.write(str(ydork) + '\n')
+        ysearch.set_headers({'User-Agent': uagents})
+        yout = ysearch.search(str(dork), cmd.page)
+        for links in yout.links():
+            if cmd.output is not None:
+                stdout.write(str(links) + '\n')
+                saveFile(cmd.output, links)
+            elif cmd.quite:
+                saveFile(cmd.output, links)
+            else:
+                stdout.write(str(links) + '\n')
+        sleep(2)
+
+
+def main():
+    try:
+        if cmd.all:
+            bingSearch()
+            # duckSearch()
+            googSearch()
+            yahoSearch()
+        else:
+            for s in engines:
+                if s == "bing":
+                    bingSearch()
+                # elif s == "duckduckgo":
+                    # duckSearch()
+                elif s == "google":
+                    googSearch()
+                elif s == "yahoo":
+                    yahoSearch()
+    except KeyboardInterrupt:
+        """
+        If ctrl+c is pressed
+        """
+        print('\n'+"CTRL+C, pressed stoping the engine")
+        exit(0)
+
+if __name__ == '__main__':
+    main()
